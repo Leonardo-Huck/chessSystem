89b57c6cf3c1dc000a7e1b481b64b082d1264cfa
Leonardo Huck <leonardomonteirohuck@gmail.com>
2025-07-20 14:34:11 -0300

    Criado a lógica de movimentação de peçãs.

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,28 @@
 package application;
 
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
-        UserInterface.printBoard(chessMatch.getPieces());
+
+        while (true) {
+            UserInterface.printBoard(chessMatch.getPieces());
+            System.out.println();
+            System.out.print("Source: ");
+            ChessPosition source = UserInterface.readChessPosition(scanner);
+
+            System.out.println();
+            System.out.print("Target: ");
+            ChessPosition target = UserInterface.readChessPosition(scanner);
+
+            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+        }
     }
 }
