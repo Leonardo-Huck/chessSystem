d4986a2074e24eb4113b804ef1bb0a6fe5660e3a
Leonardo Huck <leonardomonteirohuck@gmail.com>
2025-09-08 20:15:50 -0300

    Trocando players a cada turno

--- src/application/UserInterface.java
+++ src/application/UserInterface.java
@@ -1,5 +1,6 @@
 package application;
 
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
@@ -30,22 +31,29 @@ public class UserInterface {
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
     // https://stackoverflow.com/questions/2979383/java-clear-the-console
-    public static void clearScreen(){
+    public static void clearScreen() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
     }
 
-    public static ChessPosition readChessPosition(Scanner scanner){
+    public static ChessPosition readChessPosition(Scanner scanner) {
         try {
             String s = scanner.nextLine();
             char column = s.charAt(0);
             int row = Integer.parseInt(s.substring(1));
 
             return new ChessPosition(column, row);
-        }catch (InputMismatchException e){
+        } catch (InputMismatchException e) {
             throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8.");
         }
-        }
+    }
+
+    public static void printMatch(ChessMatch chessMatch){
+        printBoard(chessMatch.getPieces());
+        System.out.println();
+        System.out.println("Turn : " + chessMatch.getTurn());
+        System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+    }
 
     public static void printBoard(ChessPiece[][] pieces){
         for (int i = 0; i<pieces.length; i++){
